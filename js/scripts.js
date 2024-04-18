423b4023cd24bd64738ddafa6dddebd8c82145a6
hantaray <info@alexander-hantke.com>
2024-04-18 20:09:22 +0200

    updated localisation

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,30 @@
-(function () {
-    // Function to update content based on selected language
-    function updateContent(langData) {
-        document.querySelectorAll('[data-i18n]').forEach(element => {
-            const key = element.getAttribute('data-i18n');
-            element.textContent = langData[key];
-        });
-    }
 
-    // Function to fetch language data
-    async function fetchLanguageData(lang) {
+// Function to update content based on selected language
+function updateContent(langData) {
+    document.querySelectorAll('[data-i18n]').forEach(element => {
+        const key = element.getAttribute('data-i18n');
+        element.textContent = langData[key];
+    });
+}
+
+// Function to fetch language data
+async function fetchLanguageData(lang) {
+    try {
         const response = await fetch(`languages/${lang}.json`);
         return response.json();
+    } catch (error) {
+        console.error('Error fetching language data:', error);
+        throw error; // Rethrow the error to handle it elsewhere if needed
     }
+}
 
-    // Function to change language
-    async function changeLanguage(lang) {
-        console.log('lang', lang)
-        const langData = await fetchLanguageData(lang);
-        updateContent(langData);
-    }
+// Function to change language
+async function changeLanguage(lang) {
+    console.log('lang', lang)
+    const langData = await fetchLanguageData(lang);
+    updateContent(langData);
+}
 
-    window.addEventListener('DOMContentLoaded', async () => {
-        changeLanguage();
-    });
-})();
+// window.addEventListener('DOMContentLoaded', async () => {
+//     changeLanguage('de');
+// });

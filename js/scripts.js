85be52c819ff50c197786d5de0b15c6193e58120
hantaray <info@alexander-hantke.com>
2024-04-19 12:35:27 +0200

    updated localisation

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,14 @@
-
 // Function to update content based on selected language
-function updateContent(langData) {
-    document.querySelectorAll('[data-i18n]').forEach(element => {
-        const key = element.getAttribute('data-i18n');
-        element.textContent = langData[key];
+function setLanguage(language) {
+    document.querySelectorAll('[lang]').forEach(element => {
+        if (element.getAttribute('lang') == language) {
+            element.style.display = "block";
+        } else {
+            element.style.display = "none";
+        }
     });
 }
-
-// Function to fetch language data
-async function fetchLanguageData(lang) {
-    try {
-        const myRequest = new Request(`languages/${lang}.json`);
-        const response = await fetch(myRequest);
-
-        console.log('response', response);
-
-        if (!response.ok) {
-            throw new Error(`Failed to fetch language data for ${lang}`);
-        }
-
-        const data = await response.json();
-        console.log('data', data);
-        return data;
-    } catch (error) {
-        console.error('Error fetching language data:', error);
-        throw error; // Rethrow the error to handle it elsewhere if needed
-    }
-}
-
-
-// Function to change language
-async function changeLanguage(lang) {
-    console.log('lang', lang)
-    const langData = await fetchLanguageData(lang);
-    console.log('langData', langData)
-    updateContent(langData);
-}
-
+// Initial language
 window.addEventListener('DOMContentLoaded', async () => {
-    changeLanguage('de');
+    setLanguage('de');
 });

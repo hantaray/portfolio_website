11912eb5710cf2efa14f22a09714e099dde2585d
hantaray <info@alexander-hantke.com>
2024-04-19 11:56:32 +0200

    updated change language

--- js/scripts.js
+++ js/scripts.js
@@ -10,21 +10,33 @@ function updateContent(langData) {
 // Function to fetch language data
 async function fetchLanguageData(lang) {
     try {
-        const response = await fetch(`languages/${lang}.json`);
-        return response.json();
+        const myRequest = new Request(`languages/${lang}.json`);
+        const response = await fetch(myRequest);
+
+        console.log('response', response);
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch language data for ${lang}`);
+        }
+
+        const data = await response.json();
+        console.log('data', data);
+        return data;
     } catch (error) {
         console.error('Error fetching language data:', error);
         throw error; // Rethrow the error to handle it elsewhere if needed
     }
 }
 
+
 // Function to change language
 async function changeLanguage(lang) {
     console.log('lang', lang)
     const langData = await fetchLanguageData(lang);
+    console.log('langData', langData)
     updateContent(langData);
 }
 
-// window.addEventListener('DOMContentLoaded', async () => {
-//     changeLanguage('de');
-// });
+window.addEventListener('DOMContentLoaded', async () => {
+    changeLanguage('de');
+});

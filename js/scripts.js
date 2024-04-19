666112516c8ff1c0e3a35fe0fe28ef52aecefcca
hantaray <info@alexander-hantke.com>
2024-04-19 12:44:39 +0200

    hide language-button

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,29 @@
+let siteLanguage = 'de';
+
 // Function to update content based on selected language
-function setLanguage(language) {
-    document.querySelectorAll('[lang]').forEach(element => {
-        if (element.getAttribute('lang') == language) {
-            element.style.display = "block";
-        } else {
-            element.style.display = "none";
-        }
-    });
+function toggleLanguage() {
+    if (siteLanguage == 'de') {
+        document.querySelectorAll('[lang]').forEach(element => {
+            if (element.getAttribute('lang') == 'en') {
+                element.style.display = "block";
+            } else {
+                element.style.display = "none";
+            }
+        });
+        siteLanguage = 'en';
+    } else {
+        document.querySelectorAll('[lang]').forEach(element => {
+            if (element.getAttribute('lang') == 'de') {
+                element.style.display = "block";
+            } else {
+                element.style.display = "none";
+            }
+        });
+        siteLanguage = 'de';
+    }
+
 }
 // Initial language
 window.addEventListener('DOMContentLoaded', async () => {
-    setLanguage('de');
+    toggleLanguage();
 });
